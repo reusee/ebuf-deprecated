f1bc7cf8c5df92b9ccc06b5871e3ae589d6c8897
reus <reusee@gmail.com>
2014-10-15 17:03:46 +0800

    SetCursor and UnsetCursor

--- cursor.go
+++ cursor.go
@@ -14,6 +14,30 @@ func (c Cursor) Move(n int) Cursor {
 	return Cursor(n)
 }
 
+func (b *Buffer) SetCursor(offset int) {
+	exists := false
+	cursor := Cursor(offset)
+	for _, c := range b.Cursors {
+		if c == cursor {
+			exists = true
+		}
+	}
+	if !exists {
+		b.Cursors = append(b.Cursors, cursor)
+	}
+}
+
+func (b *Buffer) UnsetCursor(offset int) {
+	newCursors := make([]Cursor, 0, len(b.Cursors)-1)
+	cursor := Cursor(offset)
+	for _, c := range b.Cursors {
+		if c != cursor {
+			newCursors = append(newCursors, c)
+		}
+	}
+	b.Cursors = newCursors
+}
+
 func (b *Buffer) InsertAtCursors(bs []byte) {
 	//TODO
 }
--- cursor_test.go
+++ cursor_test.go
@@ -7,3 +7,24 @@ func TestCursorMove(t *testing.T) {
 		t.Fatal()
 	}
 }
+
+func TestSetAndUnsetCursor(t *testing.T) {
+	r := New()
+	r.SetCursor(0)
+	if len(r.Cursors) != 1 {
+		t.Fatal()
+	}
+	r.SetCursor(1)
+	r.SetCursor(1)
+	if len(r.Cursors) != 2 {
+		t.Fatal()
+	}
+	r.UnsetCursor(0)
+	if len(r.Cursors) != 1 {
+		t.Fatal()
+	}
+	r.UnsetCursor(0)
+	if len(r.Cursors) != 1 {
+		t.Fatal()
+	}
+}
