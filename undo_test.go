c63ff44b0aa5d554493adf96dc1825bdbe31c41a
reus <reusee@gmail.com>
2014-10-15 13:27:30 +0800

    add Redo test

--- undo_test.go
+++ undo_test.go
@@ -33,3 +33,19 @@ func TestUndo(t *testing.T) {
 		t.Fatal()
 	}
 }
+
+func TestRedo(t *testing.T) {
+	b := New()
+	b.SetBytes([]byte("foo"))
+	b.Undo()
+	b.Redo()
+	if !bytes.Equal(b.Bytes(), []byte("foo")) {
+		t.Fatal()
+	}
+	b.Undo()
+	b.SetBytes([]byte("bar")) // this should clear the redo states
+	b.Redo()
+	if !bytes.Equal(b.Bytes(), []byte("bar")) {
+		t.Fatal()
+	}
+}
