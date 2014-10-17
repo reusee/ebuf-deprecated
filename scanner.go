24b9a9ccba3a812b2c6644fa3c2305840106d270
reus <reusee@gmail.com>
2014-10-18 00:06:48 +0800

    Scanner

--- scanner.go
+++ scanner.go
@@ -1,3 +1,145 @@
 package ebuf
 
-type Scanner struct{}
+import (
+	"fmt"
+	"regexp/syntax"
+	"strings"
+)
+
+type Scanner struct {
+	program      *syntax.Prog
+	captureNames []string
+	threads      []*_Thread
+	pos          int
+	Captures     []Capture
+}
+
+type _Thread struct {
+	pc       uint32
+	captures map[uint32]int
+}
+
+type Capture struct {
+	Name  string
+	Begin Cursor
+	End   Cursor
+}
+
+func (b *Buffer) SetScanner(rules map[string][]string) {
+	b.Scanners = []*Scanner{
+		NewScanner(rules),
+	}
+}
+
+func NewScanner(rules map[string][]string) *Scanner {
+	exprs := []string{}
+	for name, subs := range rules {
+		exprs = append(exprs, fmt.Sprintf(`(?P<%s>%s)`, name,
+			strings.Join(subs, "|")))
+	}
+	expr := strings.Join(exprs, "|")
+
+	re, err := syntax.Parse(expr, syntax.Perl)
+	if err != nil {
+		panic(fmt.Errorf("rule syntax error %v", err))
+	}
+	capNames := re.CapNames()
+	re = re.Simplify()
+	program, _ := syntax.Compile(re)
+
+	return &Scanner{
+		program:      program,
+		captureNames: capNames,
+		threads: []*_Thread{
+			&_Thread{
+				pc:       uint32(program.Start),
+				captures: make(map[uint32]int),
+			},
+		},
+	}
+}
+
+func (s *Scanner) FeedRune(r rune, l int) {
+	threads := s.threads
+	blockingThreads := []*_Thread{}
+loop:
+	for len(threads) > 0 {
+		thread := threads[len(threads)-1]
+		threads = threads[:len(threads)-1]
+		runeConsumed := false
+		pc := thread.pc
+		inst := s.program.Inst[pc]
+	runLoop:
+		for {
+			switch inst.Op {
+			case syntax.InstAlt, syntax.InstAltMatch:
+				// new thread
+				captures := make(map[uint32]int, len(thread.captures))
+				for nameIndex, pos := range thread.captures {
+					captures[nameIndex] = pos
+				}
+				threads = append(threads, &_Thread{
+					pc:       inst.Arg,
+					captures: captures,
+				})
+				pc = inst.Out
+				inst = s.program.Inst[pc]
+			case syntax.InstCapture:
+				nameIndex := inst.Arg / 2
+				name := s.captureNames[nameIndex]
+				if name != "" { // skip nameless groups
+					if pos, ok := thread.captures[nameIndex]; ok { // end of named group
+						s.Captures = append(s.Captures, Capture{
+							Name:  s.captureNames[nameIndex],
+							Begin: Cursor(pos),
+							End:   Cursor(s.pos + l),
+						})
+						delete(thread.captures, nameIndex)
+					} else {
+						thread.captures[nameIndex] = s.pos
+					}
+				}
+				pc = inst.Out
+				inst = s.program.Inst[pc]
+			case syntax.InstEmptyWidth:
+				panic("empty string pattern is not supported")
+			case syntax.InstMatch, syntax.InstFail: // clear all threads, restart
+				blockingThreads = nil
+				break loop
+			case syntax.InstNop:
+				pc = inst.Out
+				inst = s.program.Inst[pc]
+			case syntax.InstRune1, syntax.InstRune, syntax.InstRuneAny, syntax.InstRuneAnyNotNL:
+				if runeConsumed { // thread blocks
+					thread.pc = pc
+					blockingThreads = append(blockingThreads, thread)
+					break runLoop
+				} else { // consume rune
+					if inst.Op == syntax.InstRune1 && r == inst.Rune[0] ||
+						inst.Op == syntax.InstRune && inst.MatchRune(r) ||
+						inst.Op == syntax.InstRuneAny ||
+						inst.Op == syntax.InstRuneAnyNotNL && r != '\n' { // rune matchs
+						runeConsumed = true
+						pc = inst.Out
+						inst = s.program.Inst[pc]
+					} else { // thread dies
+						break runLoop
+					}
+				}
+			}
+		}
+	}
+
+	if len(blockingThreads) > 0 {
+		s.threads = blockingThreads
+	} else { // restart
+		s.threads = []*_Thread{
+			{
+				pc:       uint32(s.program.Start),
+				captures: make(map[uint32]int),
+			},
+		}
+	}
+
+	s.pos += l
+}
--- scanner_test.go
+++ scanner_test.go
@@ -1,1 +1,108 @@
 package ebuf
+
+import "testing"
+
+func TestScanner(t *testing.T) {
+	r := New()
+	r.SetScanner(map[string][]string{
+		"foo": {"foo"},
+		"bar": {"bar"},
+		"baz": {"(b)(a)(z)"},
+	})
+	r.SetBytes([]byte(`foobarbazbazfoobar`))
+
+	scanner := r.Scanners[len(r.Scanners)-1]
+	r.rope.IterRune(0, func(ru rune, l int) bool {
+		scanner.FeedRune(ru, l)
+		return true
+	})
+
+	expected := []Capture{
+		{"foo", 0, 3},
+		{"bar", 3, 6},
+		{"baz", 6, 9},
+		{"baz", 9, 12},
+		{"foo", 12, 15},
+		{"bar", 15, 18},
+	}
+	for i, c := range scanner.Captures {
+		if c != expected[i] {
+			t.Fatal()
+		}
+	}
+
+	func() {
+		defer func() {
+			if err := recover(); err == nil {
+				t.Fatal()
+			}
+		}()
+		r.SetScanner(map[string][]string{
+			"foo": {"["},
+		})
+	}()
+
+	r.SetScanner(map[string][]string{
+		"foo": {"foo", "bar", "baz"},
+	})
+	scanner = r.Scanners[len(r.Scanners)-1]
+	r.rope.IterRune(0, func(ru rune, l int) bool {
+		scanner.FeedRune(ru, l)
+		return true
+	})
+
+	expected = []Capture{
+		{"foo", 0, 3},
+		{"foo", 3, 6},
+		{"foo", 6, 9},
+		{"foo", 9, 12},
+		{"foo", 12, 15},
+		{"foo", 15, 18},
+	}
+	for i, c := range scanner.Captures {
+		if c != expected[i] {
+			t.Fatal()
+		}
+	}
+
+	func() {
+		defer func() {
+			if err := recover(); err == nil {
+				t.Fatal()
+			}
+		}()
+		r.SetScanner(map[string][]string{
+			"foo": {"^$"},
+		})
+		scanner = r.Scanners[len(r.Scanners)-1]
+		r.rope.IterRune(0, func(ru rune, l int) bool {
+			scanner.FeedRune(ru, l)
+			return true
+		})
+	}()
+
+	r.SetScanner(map[string][]string{
+		"foo": {"()"},
+	})
+	scanner = r.Scanners[len(r.Scanners)-1]
+	r.rope.IterRune(0, func(ru rune, l int) bool {
+		scanner.FeedRune(ru, l)
+		return true
+	})
+	if len(scanner.Captures) != 18 {
+		t.Fatal()
+	}
+
+	r.SetScanner(map[string][]string{
+		"foo": {".*"},
+	})
+	scanner = r.Scanners[len(r.Scanners)-1]
+	r.rope.IterRune(0, func(ru rune, l int) bool {
+		scanner.FeedRune(ru, l)
+		return true
+	})
+	if len(scanner.Captures) != 18 {
+		t.Fatal()
+	}
+
+}
